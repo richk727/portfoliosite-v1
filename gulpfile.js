b2a484c4a991f4a9092b62cd05a6f3af881d30a8
Richard Kaye <richk727@gmail.com>
2020-02-23 20:12:28 +0000

    Update file structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,57 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const rename = require("gulp-rename");
+const cleanCSS = require("gulp-clean-css");
 
-const browserSync = require('browser-sync');
+const browserSync = require("browser-sync");
 
 const server = browserSync.create();
 
+sass.compiler = require("node-sass");
 
 const paths = {
-  styles: {
-    src: 'dev/sass/**/*.sass',
-    dest: 'public/assets/css/'
-  },
-  scripts: {
-    src: 'deb/js/**/*.js',
-    dest: 'public/assets/js/'
-  }
+	styles: {
+		src: "dev/sass/**/*.scss",
+		dest: "public/assets/css/"
+	},
+	scripts: {
+		src: "dev/js/**/*.js",
+		dest: "public/assets/js/"
+	}
 };
- 
+
 /*
  * Define our tasks using plain functions
  */
 function styles() {
-  return gulp.src(paths.styles.src)
-  	.pipe(sass().on('error', sass.logError))
-    .pipe(cleanCSS())
-    // pass in options to the stream
-    .pipe(rename({
-      basename: 'main',
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest(paths.styles.dest));
+	return (
+		gulp
+			.src(paths.styles.src)
+			.pipe(sass().on("error", sass.logError))
+			.pipe(cleanCSS())
+			// pass in options to the stream
+			.pipe(
+				rename({
+					basename: "main",
+					suffix: ".min"
+				})
+			)
+			.pipe(gulp.dest(paths.styles.dest))
+	);
 }
- 
+
 function scripts() {
-  return gulp.src(paths.scripts.src, { sourcemaps: false })
-    .pipe(uglify())
-    .pipe(concat('main.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest));
+	return gulp
+		.src(paths.scripts.src, { sourcemaps: false })
+		.pipe(uglify())
+		.pipe(concat("main.min.js"))
+		.pipe(gulp.dest(paths.scripts.dest));
 }
- 
+
 function reload(done) {
 	server.reload();
 	done();
@@ -52,20 +59,18 @@ function reload(done) {
 
 function serve(done) {
 	server.init({
-	  server: {
-		baseDir: 'public/'
-	  }
+		server: {
+			baseDir: "public/"
+		}
 	});
 	done();
 }
 
 function watch() {
-  gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
-  gulp.watch(paths.styles.src, gulp.series(styles, reload));
+	gulp.watch(paths.scripts.src, gulp.series(scripts, reload));
+	gulp.watch(paths.styles.src, gulp.series(styles, reload));
 }
 
-
- 
 /*
  * Specify if tasks run in series or parallel using `gulp.series` and `gulp.parallel`
  */
